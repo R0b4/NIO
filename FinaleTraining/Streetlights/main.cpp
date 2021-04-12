e9e641e8ff5d60fe812a2791b430febd4e0b75d7
Rob <151093rb@gmail.com>
2021-04-12 15:42:50 +0200

    Finale Training

---
+++ FinaleTraining/Streetlights/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <queue>
+
+struct Junction
+{
+	int parent;
+	int setsize;
+};
+
+struct street{
+	int x;
+	int y;
+
+	int distance;
+};
+
+struct Compare{
+	bool operator()(const street &a, const street &b){
+		return a.distance > b.distance;
+	}
+};
+
+int findParent(Junction *list, int a){
+	for (;;){
+		if (list[a].parent == a) return a;
+		else a = list[a].parent;
+	}
+}
+
+bool addTo(Junction *list, int a, int b){
+	a = findParent(list, a);
+	b = findParent(list, b);
+
+	if (a == b) return false;
+
+	if (list[a].setsize < list[b].setsize) list[a].parent = b;
+	else list[b].parent = a;
+
+	return true;
+}
+
+int doLights(int n, int m, std::priority_queue<street, std::vector<street>, Compare> &streets){
+	int savings = 0;
+
+	Junction *j = new Junction[n];
+	for (int i = 0; i < n; i++) {
+		j[i].parent = i;
+		j[i].setsize = 1;
+	}
+
+	for (street s = streets.top(); streets.size() != 0; streets.pop(), s = streets.top()){
+		printf("%i %i %i\n", s.x, s.y, s.distance);
+
+		if (!addTo(j, s.x, s.y)){
+			savings += s.distance;
+
+			continue;
+		}
+	}
+
+	return savings;
+}
+
+int main(){
+	for (;;){
+		int m, n;
+		scanf("%i %i", &m, &n);
+		if (m == 0 && n == 0) return 0;
+
+		std::priority_queue<street, std::vector<street>, Compare> streets;
+		for (int i = 0; i < n; i++){
+			street s;
+			scanf("%i %i %i", &s.x, &s.y, &s.distance);
+
+			streets.push(s);
+		}
+
+		printf("%i\n", doLights(m, n, streets));
+	}
+}
